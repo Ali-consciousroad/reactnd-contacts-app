ff5eaa755656632c82d7f9b921f159aa1cd5706d
Ali-consciousroad <ali_dindar@live.be>
2021-08-20 10:48:45 +0200

    Display contact avatar, name  and the remove button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -7,8 +7,25 @@ class ListContacts extends Component {
         return (
             <ol className='contact-list'>
                 {this.props.myContact.map((contact) => (
-                    <li key={contact.id}>
-                        {contact.name}
+                    /* id is used by React to work more efficiently.
+                    If one of the list item change, React can use the id to know that instead of 
+                    rendering the list at every change */
+                    <li key={contact.id} className='contact-list-item'>
+                        {/* Use the contact image as background image */}
+                        <div
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}
+                        ></div>
+                        {/* Render the name and the handle */}
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>
                     </li>
                 ))}
             </ol>
