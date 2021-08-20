61d4a2f5accff73bbe80f6ee47c858a6b319afa1
Ali-consciousroad <ali_dindar@live.be>
2021-08-20 09:25:32 +0200

    Add default contacts and the ListContacts component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-
+import ListContacts from './ListContacts';
+// Currently with take the contacts from the FE but later we'll learn to take the contact data from the BE
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts myContact={contacts}/>
       </div>
     );
   }
